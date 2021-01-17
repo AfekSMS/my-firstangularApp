ce2bb56d41ca4cd8f615853d4043ef007daae6c8
Afek <afek@sms2010.co.il>
2021-01-17 16:43:46 +0200

    added a new component by the name server

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core'
+import { Server } from 'http';
+
+@Component({
+    selector: 'app-server',
+    templateUrl:'./Server.component.html'
+})
+
+export class ServerComponent{
+
+}
+
